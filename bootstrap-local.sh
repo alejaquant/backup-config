6619e8d8e6a463c5a7bd2c6e5f08fb287135794a
María Alejandra Martínez <alejquant@gmail.com>
2026-03-17 17:36:41 -0400

    Comment out tools installation script
    Comment out the installation of tools in bootstrap-local.sh

--- bootstrap-local.sh
+++ bootstrap-local.sh
@@ -7,7 +7,7 @@ echo "Starting system setup..."
 #bash scripts/install-base.sh
 #bash brew/install-brew.sh
 #bash scripts/install-zsh.sh
-bash scripts/install-tools.sh
+#bash scripts/install-tools.sh
 bash git/setup-git.sh
 bash scripts/install-ssh.sh
 bash scripts/install-languages.sh
