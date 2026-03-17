7e9412b6783bb0666419fa6dd2898026f7f7d912
María Alejandra Martínez <alejquant@gmail.com>
2026-03-17 17:26:22 -0400

    Comment out Zsh installation in bootstrap-local.sh
    Comment out the installation of Zsh in the bootstrap script.

--- bootstrap-local.sh
+++ bootstrap-local.sh
@@ -6,7 +6,7 @@ echo "Starting system setup..."
 
 #bash scripts/install-base.sh
 #bash brew/install-brew.sh
-bash scripts/install-zsh.sh
+#bash scripts/install-zsh.sh
 bash scripts/install-tools.sh
 bash git/setup-git.sh
 bash scripts/install-ssh.sh
