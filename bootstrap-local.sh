21f819977741a745105993c47ef0655508950fb1
María Alejandra Martínez <alejquant@gmail.com>
2026-03-17 17:15:10 -0400

    Disable base installation scripts in bootstrap-local.sh
    Comment out base installation scripts for local setup.

--- bootstrap-local.sh
+++ bootstrap-local.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "Starting system setup..."
 
-bash scripts/install-base.sh
-bash brew/install-brew.sh
+#bash scripts/install-base.sh
+#bash brew/install-brew.sh
 bash scripts/install-zsh.sh
 bash scripts/install-tools.sh
 bash git/setup-git.sh
